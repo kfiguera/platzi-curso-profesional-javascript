33d4e21be01eb547ba98166b034f6d5845dc968b
Kevin Figuera <kevinalejandro3@gmail.com>
2021-12-12 23:22:22 -0400

    Scope

--- assets/index.js
+++ assets/index.js
@@ -1,27 +1,10 @@
+import MediaPlayer from "./MediaPlayer.js";
 
 const video = document.querySelector("video");
 const button = document.querySelector("button");
 
-class MediaPlayer {
-    constructor(config) {
-        this.media = config.el;
-    }
-
-    play() {
-        this.media.play();
-        this.media.paused
-    }
-
-    pause() {
-        this.media.paused();
-    }
-
-    togglePlay() {
-        (this.media.paused)
-            ? this.media.play()
-            : this.media.pause();
-    }
-}
-
 const player = new MediaPlayer({el: video});
-button.onclick = () => player.togglePlay();
+
+button.onclick = () => {
+    player.togglePlay();
+}
