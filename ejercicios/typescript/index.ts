0018c0819988c8b24d2ea60dbfdca73d89a47c29
Kevin Figuera <kevinalejandro3@gmail.com>
2022-01-07 17:35:31 -0400

    Interfaces

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,21 +1,30 @@
-//Funciones
-function add(a: number, b: number): number {
-    return a + b;
+// Interfaces
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+    Azul = "Azul"
 }
 
-const sum = add(4, 6);
-
-function createAdder(a: number): (number) => number {
-    return function (b: number) {
-        return b + a;
-    }
+interface Rectangulo {
+    ancho: number
+    alto: number
+    color?: Color
 }
 
-const addFour = createAdder(4);
-const fourPlus6 = addFour(6);
-
-function fullName(firstName:string, lastName: string = 'Smith'): string {
-    return `${firstName} ${lastName}`;
+let rect: Rectangulo = {
+    ancho: 4,
+    alto: 6,
+    //color: Color.Rojo
 }
-const kevin = fullName('Kevin');
-console.log(kevin)
+
+function area(r: Rectangulo): number {
+    return r.alto * r.ancho;
+}
+
+const areaRect = area(rect);
+console.log(areaRect);
+
+rect.toString = function () {
+    return this.color ? `Un rectangulo ${this.color}` : 'Un Rectangulo';
+}
+console.log(rect.toString());
