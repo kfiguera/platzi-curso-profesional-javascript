51f9be7cd8585777f2088ed8069fcb3eefa04bf9
Kevin Figuera <kevinalejandro3@gmail.com>
2022-01-07 17:10:58 -0400

    Tipos básicos

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,39 @@
-console.log('hello ts')
-function add(a: number,b: number) {
-return a + b;
+// Boolean
+let muted: boolean = true;
+//muted = 'Hola';
+muted = false;
+
+//Números
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = 'Kevin';
+let saludo = `Me llamo ${nombre}`;
+
+// Arreglos
+let people: string[] = [];
+people = ['Isabel', 'Nicole', 'Raul'];
+//people.push("9000");
+
+let peopleAndNumbers: Array< string | number > = [];
+peopleAndNumbers.push('Ricardo');
+peopleAndNumbers.push(9000);
+
+//enum
+enum Color {
+    Rojo= "Rojo",
+    Verde = "Verde",
+    Azul = "Azul",
 }
-const sum = add(2,3);
-const sum = add('a',3);
-console.log(sum);
+let colorFavorito: Color = Color.Rojo;
+console.log(`Mi color Favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = "joker";
+comodin = {type: "wildcard"};
+
+//Object
+let someObject: object = {type: "wildcard"};
