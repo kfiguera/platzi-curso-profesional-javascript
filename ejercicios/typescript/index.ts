ccfab991301b15f52b4d7c8bd7a2126c8cd41105
Kevin Figuera <kevinalejandro3@gmail.com>
2022-01-07 17:26:38 -0400

    Funciones

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,39 +1,21 @@
-// Boolean
-let muted: boolean = true;
-//muted = 'Hola';
-muted = false;
-
-//Números
-let age = 6;
-let numerador: number = 42;
-let denominador: number = age;
-let resultado = numerador / denominador;
-
-// String
-let nombre: string = 'Kevin';
-let saludo = `Me llamo ${nombre}`;
-
-// Arreglos
-let people: string[] = [];
-people = ['Isabel', 'Nicole', 'Raul'];
-//people.push("9000");
-
-let peopleAndNumbers: Array< string | number > = [];
-peopleAndNumbers.push('Ricardo');
-peopleAndNumbers.push(9000);
-
-//enum
-enum Color {
-    Rojo= "Rojo",
-    Verde = "Verde",
-    Azul = "Azul",
+//Funciones
+function add(a: number, b: number): number {
+    return a + b;
 }
-let colorFavorito: Color = Color.Rojo;
-console.log(`Mi color Favorito es ${colorFavorito}`);
 
-// Any
-let comodin: any = "joker";
-comodin = {type: "wildcard"};
+const sum = add(4, 6);
 
-//Object
-let someObject: object = {type: "wildcard"};
+function createAdder(a: number): (number) => number {
+    return function (b: number) {
+        return b + a;
+    }
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+function fullName(firstName:string, lastName: string = 'Smith'): string {
+    return `${firstName} ${lastName}`;
+}
+const kevin = fullName('Kevin');
+console.log(kevin)
