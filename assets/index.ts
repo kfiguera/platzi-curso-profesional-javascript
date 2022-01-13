a5b9d7a443b5d4cb532c0281403616459e3b96da
Kevin Figuera <kevinalejandro3@gmail.com>
2022-01-12 20:45:54 -0400

    Implementación de plugin de Ads: Desplegando en pantalla

--- assets/index.ts
+++ assets/index.ts
@@ -1,12 +1,12 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
-import AdsPlugin from "./plugins/Ads";
+import Ads from './plugins/Ads';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay(), new AutoPause(), new AdsPlugin()],
+  plugins: [new AutoPlay(), new AutoPause(), new Ads()],
 });
 
 const playButton: HTMLElement = document.querySelector('#playButton');
